3456cdd1f39c3d9ce75681ba6acf743ad2a66582
Ghost <somemail@mail.com>
2021-05-30 22:47:43 +0200

    ...

---
+++ program/configurations/Remember11.py
@@ -0,0 +1,37 @@
+
+
+
+####################################################
+# 				Konfiguration				       #
+####################################################
+
+
+
+textAreaSize = 						(1750, 547)
+multiParagraphHeight =				700
+upscaleFactor =					    1
+
+customOffsetTop =					220
+customOffsetLeft =					90
+
+customOffsetTopMultiParagraph =		10
+customOffsetLeftMultiParagraph =	100
+
+lineMargin =						80
+
+fadeInSpeed = 						40
+fadeOutSpeed = 						60
+framerate =							30
+waitingTime =						15
+
+fontPath =							"../fonts/R11_Narrow.ttf"
+fontSize =							60
+textColor = 						(0, 0, 0)
+
+scriptPath =						"../script_resources/Remember11.ods"
+voiceTrackResourcesFolder =			"../voice_track_resources/Fools_3/"
+
+customOutputFolderVideoClips = 		"../animated_text/videos_version_0"
+customOutputFolderVoiceTracks = 	"/home/gits/Dokumente/Dark Sky/Kdenlive/Text/voice_tracks/"
+
+indexPositions =					10
